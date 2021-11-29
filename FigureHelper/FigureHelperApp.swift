fbc54d00b2edc398165bb60b3bb44cd1279ad235
Russell Gordon <github@russellgordon.ca>
2021-11-29 09:11:05 -0500

    Initial Commit

---
+++ FigureHelper/FigureHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigureHelperApp.swift
+//  FigureHelper
+//
+//  Created by Russell Gordon on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct FigureHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
